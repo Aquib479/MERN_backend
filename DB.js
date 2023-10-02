d452ed669a29a30b38b734c6a36114cc0b35a63d
Aquib479 <mdaquib479@gmail.com>
2023-10-02 10:14:07 +0530

    new changes

--- DB.js
+++ DB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectToMongo = () => {
-    mongoose.connect('mongodb://localhost:27017/Test', {
+    mongoose.connect('mongodb+srv://mdaquib479:Mohammad786@cluster0.xbx47ho.mongodb.net/test', {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
